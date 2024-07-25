f3351945ea5b2155ddb78c0ef1b4d6bbd2e56f78
prabhavsetia <prabhav.setia21@gmail.com>
2024-07-25 14:43:45 +0530

    Adding : Navbar & Shop component

---
+++ src/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <div className="container my-3">
+                <h2>Buy Shoes 50Rs</h2>
+                <button className="btn btn-primary mx-2" >-</button>
+                Buy this item
+                <button className="btn btn-primary mx-2" >+</button>
+            </div>
+        </div>
+    )
+}
+
+export default Shop
